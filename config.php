1a8c54e4918df2ac1f0fd2d061ca45f135ba0068
noahcollin <81028212+noahcollin@users.noreply.github.com>
2023-11-30 23:18:23 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'trabalhoFormulario';
+
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+ //   if ($conexao->connect_error) {
+  //      echo'Erro! Por favor, tente novamente.';
+  //  }else{
+  //      echo'Conexao Feita';
+ //   }
+
+
+?>
